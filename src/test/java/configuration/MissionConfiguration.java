ff1147127d351ca49491b29ce3cb99cf22eb7830
Maged Mamdouh <Maged.mamdouh@mondiamedia.com>
2017-12-16 21:58:47 +0200

    Add methods in mission configuration

--- src/test/java/configuration/MissionConfiguration.java
+++ src/test/java/configuration/MissionConfiguration.java
@@ -1,5 +1,31 @@
 package configuration;
 
+import pageobjects.MissonPageObject;
+import pageobjects.RoadMapPageObject;
+
 public class MissionConfiguration {
+
+    MissonPageObject missionPage;
+
+    RoadMapPageObject roadMapPage;
+
+    public MissionConfiguration(MissonPageObject missionPage, RoadMapPageObject roadMapPage) {
+        this.missionPage = missionPage;
+        this.roadMapPage = roadMapPage;
+    }
+
+    public String failedMissionCheck() {
+        missionPage.closeMissionHint().clickRunButton();
+        String text = missionPage.dialogueText();
+        missionPage.clickContinueButton();
+        return text;
+    }
+    
+    public void missionCheck(){
+        missionPage.clickSettingButton()
+        .clickModelAnswerButton()
+        .clickRunButton()
+        .clickContinueButton();
+    }
 
 }
