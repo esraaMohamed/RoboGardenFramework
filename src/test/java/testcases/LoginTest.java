e61b35a993618aab73265f9423feda62730fff0c
Maged Mamdouh <Maged.mamdouh@mondiamedia.com>
2017-12-16 20:34:45 +0200

    Add invalid login method and data update pom file formatting and selenium version add geckodriver and chrome driver compatible with machine

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -8,37 +8,56 @@ import configuration.LoginConfiguration;
 import pageobjects.HomePageObject;
 import pageobjects.LoginPageObject;
 
+/**
+ * This class contains the test cases of the login and logout functionality
+ */
+public class LoginTest extends BaseTest {
 
-/** 
- * This class contains the test cases of the login and logout functionality*/
-public class LoginTest extends BaseTest{
-	LoginPageObject loginPage;
-	LoginConfiguration loginConfiguration;
-	HomePageObject homePage;
-	String username , password;
-	
-	@BeforeTest
-	public void init() {
-		loginPage = new LoginPageObject(driver);
-		homePage = new HomePageObject(driver);
-		loginConfiguration = new LoginConfiguration(loginPage, homePage);
-		username = jsonTestData.getData("Login").get("username");
-		password = jsonTestData.getData("Login").get("password");
-	}
+    LoginPageObject loginPage;
 
-	/**
-	 * Verifying login functionality*/
-	@Test(priority = 1)
-	public void login() {
-		loginConfiguration.login(username, password);
-		Assert.assertTrue("Login Failed", loginConfiguration.isUserLoggedIn());
-	}
-	
-	/**
-	 * Verifying logout functionality*/
-	@Test(priority = 2)
-	public void logout() {
-		loginConfiguration.logout();
-		Assert.assertTrue("Logout Failed", driver.getCurrentUrl().contains("home"));
-	}
+    LoginConfiguration loginConfiguration;
+
+    HomePageObject homePage;
+
+    String username, password, failedUsername, failedPassword;
+
+    @BeforeTest
+    public void init() {
+        loginPage = new LoginPageObject(driver);
+        homePage = new HomePageObject(driver);
+        loginConfiguration = new LoginConfiguration(loginPage, homePage);
+        username = jsonTestData.getData("Login").get("username");
+        password = jsonTestData.getData("Login").get("password");
+        failedUsername = jsonTestData.getData("invalidLogin").get("username");
+        failedPassword = jsonTestData.getData("invalidLogin").get("password");
+
+    }
+
+    /**
+     * Verifying invalid login functionality
+     */
+    @Test(priority = 1)
+    public void invalidLogin() {
+        loginConfiguration.invalidlogin(username, password);
+        //TODO
+        Assert.assertTrue("Login Failed", loginConfiguration.isUserLoggedIn());
+    }
+
+    /**
+     * Verifying login functionality
+     */
+    @Test(priority = 2)
+    public void login() {
+        loginConfiguration.login(username, password);
+        Assert.assertTrue("Login Failed", loginConfiguration.isUserLoggedIn());
+    }
+
+    /**
+     * Verifying logout functionality
+     */
+    @Test(priority = 3)
+    public void logout() {
+        loginConfiguration.logout();
+        Assert.assertTrue("Logout Failed", driver.getCurrentUrl().contains("home"));
+    }
 }
