d0a3547399740a5edef9c3067a90406e6b5860c0
Maged Mamdouh <Maged.mamdouh@mondiamedia.com>
2017-12-16 21:25:59 +0200

    Add Mission page object data and methods

--- src/test/java/pageobjects/MissonPageObject.java
+++ src/test/java/pageobjects/MissonPageObject.java
@@ -1,12 +1,59 @@
 package pageobjects;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
-public class MissonPageObject extends BasePage{
+public class MissonPageObject extends BasePage {
 
-	public MissonPageObject(WebDriver driver) {
-		super(driver);
-		// TODO Auto-generated constructor stub
-	}
+    @FindBy(css = ".close.ng-scope")
+    private WebElement missionTutorial;
+
+    @FindBy(id = "actions")
+    private WebElement actions;
+
+    @FindBy(id = "modelanswer")
+    private WebElement modelAnswer;
+
+    @FindBy(id = "run_button")
+    private WebElement runButton;
+
+    @FindBy(css = ".modal-title.ng-binding")
+    private WebElement popupTitle;
+
+    @FindBy(id = "check_answer_button")
+    private WebElement checkAnswerButton;
+
+    public MissonPageObject(WebDriver driver) {
+        super(driver);
+    }
+
+    public MissonPageObject closeMissionHint() {
+        click(missionTutorial);
+        return this;
+    }
+
+    public MissonPageObject clickSettingButton() {
+        click(actions);
+        return this;
+    }
+
+    public MissonPageObject clickModelAnswerButton() {
+        click(modelAnswer);
+        return this;
+    }
+
+    public MissonPageObject clickRunButton() {
+        click(runButton);
+        return this;
+    }
+
+    public String dialogueText() {
+        return popupTitle.getText();
+    }
+
+    public void clickContinueButton() {
+        click(checkAnswerButton);
+    }
 
 }
