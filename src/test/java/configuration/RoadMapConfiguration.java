b8c2047f4ef681e3ac62ce1ed85abf8fab048fd9
Maged Mamdouh <Maged.mamdouh@mondiamedia.com>
2017-12-18 18:51:04 +0200

    add missing file in the previous commit

--- src/test/java/configuration/RoadMapConfiguration.java
+++ src/test/java/configuration/RoadMapConfiguration.java
@@ -4,32 +4,35 @@ import pageobjects.MissionPageObject;
 import pageobjects.RoadMapPageObject;
 
 public class RoadMapConfiguration {
-	RoadMapPageObject roadMapPage;
-	MissionConfiguration missionConfiguration;
 
-	public RoadMapConfiguration(RoadMapPageObject roadMapPage, MissionPageObject missionPage) {
-		this.roadMapPage = roadMapPage;
-		this.missionConfiguration = new MissionConfiguration(missionPage, roadMapPage);
-	}
+    RoadMapPageObject roadMapPage;
 
-	public void closeHint() {
-		roadMapPage.closeHintWindow();
-	}
+    MissionConfiguration missionConfiguration;
 
+    String failedText, successText;
 
-	public void clickOnMission() {
-		roadMapPage.clickSlickDotByIndex(0);
-		roadMapPage.clickMissionByIndex(0);
-		int missionsSize = roadMapPage.getMissionsSize();
-		for (int i = 0; i < missionsSize; i++) {
-			// every 5 missions will do the following
-			if (i != 0 && i % 5 == 0) {
-				roadMapPage.clickNextSlickDot();
-				roadMapPage.clickMissionByIndex(i);
-			}
-			missionConfiguration.failedMissionCheck();
-			missionConfiguration.missionCheck();
+    public RoadMapConfiguration(RoadMapPageObject roadMapPage, MissionPageObject missionPage) {
+        this.roadMapPage = roadMapPage;
+        this.missionConfiguration = new MissionConfiguration(missionPage, roadMapPage);
+    }
 
-		}
-	}
+    public void closeHint() {
+        roadMapPage.closeHintWindow();
+    }
+
+    public void clickOnMission() {
+        roadMapPage.clickSlickDotByIndex(0);
+        roadMapPage.clickMissionByIndex(0);
+        int missionsSize = roadMapPage.getMissionsSize();
+        for (int i = 0; i < missionsSize; i++) {
+            // every 5 missions will do the following
+            if (i != 0 && i % 5 == 0) {
+                roadMapPage.clickNextSlickDot();
+                roadMapPage.clickMissionByIndex(i);
+            }
+            failedText = missionConfiguration.failedMissionCheck();
+            successText = missionConfiguration.missionCheck();
+
+        }
+    }
 }
